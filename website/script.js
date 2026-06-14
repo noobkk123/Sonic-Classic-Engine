6505937b15678ca331ce1502136e0756ba92fca1
noobkk123 <carvalhokallebe0@gmail.com>
2026-06-14 15:47:40 -0300

    Delete website directory
    L github do not detect the index.html

--- website/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const botao = document.getElementById('colors');
-
-  if (botao) {
-    botao.addEventListener('click', () => {
-      document.documentElement.classList.toggle('light');
-    });
-  }
-});
